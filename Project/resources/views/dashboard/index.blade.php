37b099785892f196dc302b34b9c0ab29fa6b11bf
Ahmadcodingalx <moroumamam53@gmail.com>
2025-03-13 22:58:13 +0000

    modification 1 du diagrame de classe

--- Project/resources/views/dashboard/index.blade.php
+++ Project/resources/views/dashboard/index.blade.php
@@ -14,8 +14,8 @@
                         <div class="card-body p-20">
                             <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                                 <div>
-                                    <p class="fw-medium text-primary-light mb-1">Total Users</p>
-                                    <h6 class="mb-0">20,000</h6>
+                                    <p class="fw-medium text-primary-light mb-1">Total des utlisateurs</p>
+                                    <h6 class="mb-0">25,000</h6>
                                 </div>
                                 <div class="w-50-px h-50-px bg-cyan rounded-circle d-flex justify-content-center align-items-center">
                                     <iconify-icon icon="gridicons:multiple-users" class="text-white text-2xl mb-0"></iconify-icon>
@@ -23,7 +23,7 @@
                             </div>
                             <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                                 <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                                    <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> +5000
+                                    <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> +200
                                 </span>
                                 Last 30 days users
                             </p>
